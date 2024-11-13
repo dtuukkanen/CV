590908eb8c52972131dc324dd4cf7596f53e0e17
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2024-11-13 15:05:12 +0200

    Enhance CV layout with new styling and additional information

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fira': ['Fira Sans', 'sans-serif'],
+      },
+      colors: {
+        primary: "#021526",
+        secondary: "#03346E",
+        accent: "#6EACDA",
+        text: "#E2E2B6",
+      },
+    },
   },
   plugins: [],
 }
